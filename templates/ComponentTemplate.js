561c10e37e216903b759276020fbc4c4a8c33ed4
Nikola Stankovic <disa_malii@hotmail.com>
2023-04-01 21:35:55 +0200

    template style

--- templates/ComponentTemplate.js
+++ templates/ComponentTemplate.js
@@ -10,36 +10,35 @@ export const ComponentTemplate = (templateName, type, lang = '', scss = false, s
   let scriptTag;
   if (type === 'composition') {
     scriptTag = `
-    <script${lang ? ` lang="${lang}"` : ''} setup>
-      /* Composition scripts here */
-    </script>`;
+<script${lang ? ` lang="${lang}"` : ''} setup>
+  /* Composition scripts here */
+</script>`;
   } else if (type === 'options') {
     scriptTag = `
-    <script${lang ? ` lang="${lang}"` : ''}>
-      export default {
-        data() {
-          return {
-            /* Data options here */
-          };
-        }
-      }
-    </script>
+<script${lang ? ` lang="${lang}"` : ''}>
+  export default {
+    data() {
+      return {
+        /* Data options here */
+      };
+    }
+  }
+</script>
     `;
   }
 
-  return `
-  <template>
-    <div class="${templateName.toLowerCase()}">
-      <h1>${templateName}</h1>
-    </div>
-  </template>
+  return `<template>
+  <div class="${templateName.toLowerCase()}">
+    <h1>${templateName}</h1>
+  </div>
+</template>
 
-  ${scriptTag}
-  
-  <style${scss ? ` lang="scss"` : ''} ${scoped ? ' scoped' : ''}>
-    .${templateName.toLowerCase()}{
-      /* Styling here */
-    }
-  </style>
+${scriptTag}
+
+<style${scss ? ` lang="scss"` : ''}${scoped ? ` scoped` : ''}>
+  .${templateName.toLowerCase()}{
+    /* Styling here */
+  }
+</style>
   `;
 };
